d70e0af1bfd8318d3dbc29ce1428bc302901cf1e
lemonliu1993 <2305247091@qq.com>
2020-01-25 17:18:49 +0800

    Spring-注解驱动开发框架

---
+++ spring-annotation/src/main/java/com/lemon/bean/Person.java
@@ -0,0 +1,12 @@
+package com.lemon.bean;
+
+import lombok.Data;
+
+/**
+ * Created by lemoon on 20/1/25 下午5:15
+ */
+@Data
+public class Person {
+    private String name;
+    private Integer age;
+}
