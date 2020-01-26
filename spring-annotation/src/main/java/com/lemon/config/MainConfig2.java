146599e24620c6d2634db1ca042e4b86904f8768
lemonliu1993 <2305247091@qq.com>
2020-01-27 00:14:10 +0800

    @Lazy懒加载

--- spring-annotation/src/main/java/com/lemon/config/MainConfig2.java
+++ spring-annotation/src/main/java/com/lemon/config/MainConfig2.java
@@ -3,6 +3,7 @@ package com.lemon.config;
 import com.lemon.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 /**
@@ -27,10 +28,16 @@ public class MainConfig2 {
      *          以后每次获取就是直接从容器(map.get)中拿。
      * request:同一次请求创建一个实例
      * session：同一个session创建一个实例
+     *
+     * 懒加载：
+     *  单实例bean，默认再容器启动的时候创建对象
+     *  懒加载：容器启动不创建对象，第一次使用(获取)Bean创建对象，并初始化。
+     *
      */
     //@Scope 调整作用域
-    @Scope("prototype")
+//    @Scope("prototype")
 //    @Scope
+    @Lazy
     @Bean("person")
     public Person person() {
         System.out.println("给容器中添加Person......");
