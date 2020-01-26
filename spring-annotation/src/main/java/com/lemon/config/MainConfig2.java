df6ed0bb5cb3d66c7e8213b2020f34ece28f327c
lemonliu1993 <2305247091@qq.com>
2020-01-26 23:45:32 +0800

    scope设置组件作用域

---
+++ spring-annotation/src/main/java/com/lemon/config/MainConfig2.java
@@ -0,0 +1,39 @@
+package com.lemon.config;
+
+import com.lemon.bean.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Created by lemoon on 20/1/26 下午2:17
+ */
+
+@Configuration
+public class MainConfig2 {
+    //默认是单实例的
+
+    /**
+     * ConfigurableBeanFactory#SCOPE_PROTOTYPE  prototype
+     * ConfigurableBeanFactory#SCOPE_SINGLETON
+     * org.springframework.web.context.WebApplicationContext#SCOPE_REQUEST
+     * org.springframework.web.context.WebApplicationContext#SCOPE_SESSION
+     *
+     * @return
+     *
+     * prototype:多实例的:ioc容器启动并不会去调用方法创建对象放在容器中，
+     *          每次获取的时候才会调用方法创建对象。
+     * singleton：单实例的(默认值),ioc容器启动会调用方法创建对象放到ioc容器中。
+     *          以后每次获取就是直接从容器(map.get)中拿。
+     * request:同一次请求创建一个实例
+     * session：同一个session创建一个实例
+     */
+    //@Scope 调整作用域
+    @Scope("prototype")
+//    @Scope
+    @Bean("person")
+    public Person person() {
+        System.out.println("给容器中添加Person......");
+        return new Person("张三", 24);
+    }
+}
--- spring-annotation/src/test/java/com/lemon/test/IOCTest.java
+++ spring-annotation/src/test/java/com/lemon/test/IOCTest.java
@@ -1,6 +1,7 @@
 package com.lemon.test;
 
 import com.lemon.config.MainConfig;
+import com.lemon.config.MainConfig2;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,6 +10,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Created by lemoon on 20/1/25 下午5:45
  */
 public class IOCTest {
+
+    @SuppressWarnings("resource")
+    @Test
+    public void test02(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(MainConfig2.class);
+//        String[] definitionNames = ctx.getBeanDefinitionNames();
+//        for(String name: definitionNames){
+//            System.out.println(name);
+//        }
+//
+        System.out.println("ioc容器创建完成......");
+        Object person = ctx.getBean("person");
+        Object person2 = ctx.getBean("person");
+//        System.out.println(person==person2);
+    }
+
 
 
     @SuppressWarnings("resource")
