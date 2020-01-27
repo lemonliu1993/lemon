8acffd583f93d03a04d0567de9610cfb48b089cc
lemonliu1993 <2305247091@qq.com>
2020-01-27 17:56:15 +0800

    @Bean制定初始化和销毁方法

---
+++ spring-annotation/src/main/java/com/lemon/bean/Car.java
@@ -0,0 +1,19 @@
+package com.lemon.bean;
+
+/**
+ * Created by lemoon on 20/1/27 下午3:30
+ */
+public class Car {
+
+    public Car(){
+        System.out.println("car  constructor...");
+    }
+
+    public void init(){
+        System.out.println("car ...init");
+    }
+
+    public void destroy(){
+        System.out.println("car ...destroy");
+    }
+}
