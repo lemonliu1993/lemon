478862a0bbd282d833607e399135e7ba42db7a8d
lemonliu1993 <2305247091@qq.com>
2020-01-29 21:48:14 +0800

    @Profile根据环境注册bean

--- spring-annotation/src/test/java/com/lemon/test/IOCTest_Profile.java
+++ spring-annotation/src/test/java/com/lemon/test/IOCTest_Profile.java
@@ -28,7 +28,7 @@ public class IOCTest_Profile {
 
         //1.创建一个applicationContext
         //2.设置需要激活的环境
-        applicationContext.getEnvironment().setActiveProfiles("dev");
+        applicationContext.getEnvironment().setActiveProfiles("test","dev");
         //3.注册主配置类
         applicationContext.register(MainConfigOfProfile.class);
         //4.启动刷新容器
