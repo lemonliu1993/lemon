e150cb947b1990f3e966870e2698218513907acb
lemonliu1993 <2305247091@qq.com>
2020-02-05 08:45:11 +0800

    从零开始JavaWeb——数据库连接与配置

--- chapter1/src/main/java/com/lemon/chapter2/util/PropsUtil.java
+++ chapter1/src/main/java/com/lemon/chapter2/util/PropsUtil.java
@@ -3,6 +3,9 @@ package com.lemon.chapter2.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -17,7 +20,55 @@ public class PropsUtil {
      */
     public static Properties loadProps(String fileName) {
         Properties properties = null;
+        InputStream is = null;
+        try {
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+            if (is == null) {
+                throw new FileNotFoundException(fileName + "file is not found");
+            }
+            properties = new Properties();
+            properties.load(is);
+        } catch (IOException e) {
+            LOGGER.error("load properties file failure", e);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    LOGGER.error("close input stream failure", e);
+                }
+            }
+        }
 
-        return null;
+        return properties;
     }
+
+    /**
+     * 获取字符串属性
+     *
+     * @param properties
+     * @param key
+     * @return
+     */
+    public static String getString(Properties properties, String key) {
+        return getString(properties, key, "");
+    }
+
+    /**
+     * 获取字符型属性(可指定默认值)
+     *
+     * @param properties
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    private static String getString(Properties properties, String key, String defaultValue) {
+        String value = defaultValue;
+        if (properties.containsKey(key)) {
+            value = properties.getProperty(key);
+        }
+        return value;
+    }
+
+
 }
