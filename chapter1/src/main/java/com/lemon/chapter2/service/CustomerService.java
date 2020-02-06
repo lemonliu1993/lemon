83c79ab0c74e3d497c642cd872d804b25a8ecac8
lemonliu1993 <2305247091@qq.com>
2020-02-06 10:09:06 +0800

    从零开始JavaWeb——CustomerService

--- chapter1/src/main/java/com/lemon/chapter2/service/CustomerService.java
+++ chapter1/src/main/java/com/lemon/chapter2/service/CustomerService.java
@@ -1,22 +1,80 @@
 package com.lemon.chapter2.service;
 
 import com.lemon.chapter2.model.Customer;
+import com.lemon.chapter2.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
- *
  * Created by lemoon on 20/1/20 下午5:12
  */
 public class CustomerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver", e);
+        }
+    }
+
     /**
      * 获取客户列表
+     *
      * @param keyword
      * @return
      */
-    public List<Customer> getCustomerList(String keyword){
-        //TODO
+    public List<Customer> getCustomerList(String keyword) {
+        Connection conn = null;
+        try {
+            List<Customer> customerList = new ArrayList<Customer>();
+            String sql = "SELECT * FROM customer";
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException e) {
+            LOGGER.error("execute sql failture", e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    LOGGER.error("close connection failure", e);
+                }
+            }
+        }
         return null;
     }
 
@@ -24,7 +82,7 @@ public class CustomerService {
     /**
      * 获取客户
      */
-    public Customer getCustomer(long id){
+    public Customer getCustomer(long id) {
         //TODO
         return null;
     }
@@ -32,7 +90,7 @@ public class CustomerService {
     /**
      * 创建客户
      */
-    public boolean createCustomer(Map<String,Object> fieldMap){
+    public boolean createCustomer(Map<String, Object> fieldMap) {
         //TODO
         return false;
     }
@@ -40,7 +98,7 @@ public class CustomerService {
     /**
      * 更新客户
      */
-    public boolean updateCustomer(long id,Map<String,Object> fieldMap){
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
         //TODO
         return false;
     }
@@ -48,7 +106,7 @@ public class CustomerService {
     /**
      * 删除客户
      */
-    public boolean deleteCustomer(long id){
+    public boolean deleteCustomer(long id) {
         //TODO
         return false;
     }
