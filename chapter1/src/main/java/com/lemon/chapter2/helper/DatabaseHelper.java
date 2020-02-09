3e3c4cd3b1de1110a46c2780d646c56a2c0bdf14
lemonliu1993 <2305247091@qq.com>
2020-02-09 21:26:06 +0800

    从零开始JavaWeb——DatabaseHelper

--- chapter1/src/main/java/com/lemon/chapter2/helper/DatabaseHelper.java
+++ chapter1/src/main/java/com/lemon/chapter2/helper/DatabaseHelper.java
@@ -1,20 +1,30 @@
 package com.lemon.chapter2.helper;
 
 import com.lemon.chapter2.util.PropsUtil;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 /**
  * 数据库操作助手类
  * Created by lemoon on 20/2/7 上午9:52
  */
-public final class DatabaseHelper {
+public final class DatabaseHelper<T> {
+
+    //为了确保一个线程中只有一个Connection，我们可以使用ThreadLocal来存放本地线程遍历。
+    //也就是说，将当前线程中的Connection放入ThreadLocal中存起来，这些Connection一定不会出现线程不安全问题，可以将ThreadLocal理解为一个隔离线程的容器。
+    private static final ThreadLocal<Connection> CONNECTION_HOLDER = new ThreadLocal<Connection>();
+
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
 
     private static final String DRIVER;
     private static final String URL;
@@ -40,11 +50,16 @@ public final class DatabaseHelper {
      * 获取数据库连接
      */
     public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            LOGGER.error("get connection failure", e);
+        Connection conn = CONNECTION_HOLDER.get();
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                LOGGER.error("get connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.set(conn);
+            }
         }
         return conn;
     }
@@ -52,14 +67,35 @@ public final class DatabaseHelper {
     /**
      * 关闭数据库连接
      */
-    public static void closeConnection(Connection conn) {
+    public static void closeConnection() {
+        Connection conn = CONNECTION_HOLDER.get();
         if (conn != null) {
             try {
                 conn.close();
             } catch (SQLException e) {
                 LOGGER.error("close connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.remove();
             }
         }
+    }
+
+    /**
+     * 查询实体列表
+     */
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
+        List<T> entityList;
+        try {
+            Connection conn = getConnection();
+            entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity list failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entityList;
     }
 
 
