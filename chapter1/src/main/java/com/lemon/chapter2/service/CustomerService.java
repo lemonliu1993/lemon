82a97df071bd619af6c46fe9b5814459ccdc4bf7
lemonliu1993 <2305247091@qq.com>
2020-02-14 09:37:28 +0800

    从零开始JavaWeb——CustomerService

--- chapter1/src/main/java/com/lemon/chapter2/service/CustomerService.java
+++ chapter1/src/main/java/com/lemon/chapter2/service/CustomerService.java
@@ -45,24 +45,21 @@ public class CustomerService {
      * 创建客户
      */
     public boolean createCustomer(Map<String, Object> fieldMap) {
-        //TODO
-        return false;
+        return DatabaseHelper.insertEntity(Customer.class,fieldMap);
     }
 
     /**
      * 更新客户
      */
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        //TODO
-        return false;
+        return DatabaseHelper.updateEntity(Customer.class,id,fieldMap);
     }
 
     /**
      * 删除客户
      */
     public boolean deleteCustomer(long id) {
-        //TODO
-        return false;
+        return DatabaseHelper.deleteEntity(Customer.class,id);
     }
 
 }
