d0511360a6f1981ee1b09c6af6b68e4a6843dfbb
lemonliu1993 <2305247091@qq.com>
2020-02-25 10:00:16 +0800

    虚引用 demo

---
+++ interview/src/main/java/com/lemon/session2/jvm/PhantomReferenceDemo.java
@@ -0,0 +1,29 @@
+package com.lemon.session2.jvm;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
+/**
+ * Created by lemoon on 20/2/24 下午5:02
+ */
+public class PhantomReferenceDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Object o1 = new Object();
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+        PhantomReference<Object> phantomReference = new PhantomReference<>(o1, referenceQueue);
+
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+
+        System.out.println("================");
+        o1 = null;
+        System.gc();
+        Thread.sleep(500);
+
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+
+    }
+}
