446eb3dd85dd0b7345dd89f5adf54da5b4bc4083
lemonliu1993 <2305247091@qq.com>
2020-02-25 11:02:16 +0800

    从零开始javaWeb——属性文件助手类

---
+++ smart-framework/src/main/java/com/lemon/framework/helper/ConfigHelper.java
@@ -0,0 +1,11 @@
+package com.lemon.framework.helper;
+
+import java.util.Properties;
+
+/**
+ * 属性文件助手类
+ * Created by lemoon on 20/2/24 上午9:49
+ */
+public final class ConfigHelper {
+//    private static final Properties CONFIG_PROPS=PropsUi
+}
