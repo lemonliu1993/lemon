ccf4b10a01e82e2d1d668208aae41dfa23736efb
lemonliu1993 <2305247091@qq.com>
2020-03-07 21:54:27 +0800

    从零开始javaWeb——类操作助手类

---
+++ smart-framework/src/main/java/com/lemon/framework/helper/ClassHelper.java
@@ -0,0 +1,69 @@
+package com.lemon.framework.helper;
+
+import com.lemon.framework.annotation.Controller;
+import com.lemon.framework.annotation.Service;
+import com.lemon.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类操作助手类
+ * Created by lemoon on 2020/3/7 9:39 PM
+ */
+public final class ClassHelper {
+
+    /**
+     * 定义类集合(用于存放所加载的类)
+     */
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 获取应用包名下的所有类
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取应用包名下所有Service类
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下所有Controller类
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下所有Bean类(包括：Service，Controller等)
+     */
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<Class<?>>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+
+}
