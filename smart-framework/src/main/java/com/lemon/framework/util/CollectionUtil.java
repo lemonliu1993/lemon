d3f169f67c3b95783dacfccf13bdc124109af151
lemonliu1993 <2305247091@qq.com>
2020-03-08 11:36:11 +0800

    从零开始javaWeb——数组工具类+集合工具类

---
+++ smart-framework/src/main/java/com/lemon/framework/util/CollectionUtil.java
@@ -0,0 +1,42 @@
+package com.lemon.framework.util;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * 集合工具类
+ * Created by lemoon on 20/2/6 上午9:46
+ */
+public class CollectionUtil {
+
+    /**
+     * 判断Collection是否为空
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return CollectionUtils.isEmpty(collection);
+    }
+
+    /**
+     * 判断Collection是否非空
+     */
+    public static boolean isNotEmpty(Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    /**
+     * 判断Map是否为空
+     */
+    public static boolean isEmpty(Map<?, ?> map) {
+        return MapUtils.isEmpty(map);
+    }
+
+    /**
+     * 判断Map是否非空
+     */
+    public static boolean isNotEmpty(Map<?, ?> map) {
+        return !isEmpty(map);
+    }
+}
