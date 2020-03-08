084a274a3c773b8b4acf9673bec5b019a863368c
lemonliu1993 <2305247091@qq.com>
2020-03-08 11:33:59 +0800

    从零开始javaWeb——数组工具类

---
+++ chapter1/src/main/java/com/lemon/chapter2/util/ArrayUtil.java
@@ -0,0 +1,23 @@
+package com.lemon.chapter2.util;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * 数组工具类
+ * Created by lemoon on 2020/3/8 11:28 AM
+ */
+public class ArrayUtil {
+    /**
+     * 判断数组是否非空
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+}
