4b3667eda6ed49e8abca4b88a467b4f2948ca8e9
lemonliu1993 <2305247091@qq.com>
2020-03-11 09:46:42 +0800

    从零开始javaWeb——Handler类

---
+++ smart-framework/src/main/java/com/lemon/framework/bean/Handler.java
@@ -0,0 +1,32 @@
+package com.lemon.framework.bean;
+
+import java.lang.reflect.Method;
+
+/**
+ * 封装Action信息
+ * Created by lemoon on 2020/3/11 9:40 AM
+ */
+public class Handler {
+    /**
+     * Controller类
+     */
+    private Class<?> controllerClass;
+
+    /**
+     * Action方法
+     */
+    private Method actionMethod;
+
+    public Handler(Class<?> controllerClass, Method actionMethod) {
+        this.controllerClass = controllerClass;
+        this.actionMethod = actionMethod;
+    }
+
+    public Class<?> getControllerClass() {
+        return controllerClass;
+    }
+
+    public Method getActionMethod() {
+        return actionMethod;
+    }
+}
