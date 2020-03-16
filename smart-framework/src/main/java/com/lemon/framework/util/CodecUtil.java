cdd1c8070b1f589390d04cbcbf0ac33b21c4f6b9
lemonliu1993 <2305247091@qq.com>
2020-03-16 18:14:12 +0800

    从零开始javaWeb——编码与解码操作工具类

--- smart-framework/src/main/java/com/lemon/framework/util/CodecUtil.java
+++ smart-framework/src/main/java/com/lemon/framework/util/CodecUtil.java
@@ -3,6 +3,7 @@ package com.lemon.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 /**
@@ -21,6 +22,20 @@ public final class CodecUtil {
             target = URLEncoder.encode(source, "UTF-8");
         } catch (Exception e) {
             LOGGER.error("encode url failure", e);
+            throw new RuntimeException(e);
+        }
+        return target;
+    }
+
+    /**
+     * 将URL解码
+     */
+    public static String decodeURL(String source){
+        String target;
+        try {
+            target = URLDecoder.decode(source,"UTF-8");
+        }catch (Exception e) {
+            LOGGER.error("decode url failure", e);
             throw new RuntimeException(e);
         }
         return target;
