e4ae9b46cb7d599ce00368301e62dbe7d3db0b31
lemonliu1993 <2305247091@qq.com>
2020-03-16 16:44:38 +0800

    从零开始javaWeb——请求参数对象

---
+++ smart-framework/src/main/java/com/lemon/framework/bean/Param.java
@@ -0,0 +1,31 @@
+package com.lemon.framework.bean;
+
+import com.lemon.framework.util.CastUtil;
+
+import java.util.Map;
+
+/**
+ * 请求参数对象
+ * Created by lemoon on 2020/3/16 3:54 PM
+ */
+public class Param {
+    private Map<String, Object> paramMap;
+
+    public Param(Map<String, Object> paramMap) {
+        this.paramMap = paramMap;
+    }
+
+    /**
+     * 根据参数名获取long型参数值
+     */
+    public long getLong(String name){
+        return CastUtil.castLong(paramMap.get(name));
+    }
+
+    /**
+     * 获取所有字段信息
+     */
+    public Map<String,Object> getMap(){
+        return paramMap;
+    }
+}
