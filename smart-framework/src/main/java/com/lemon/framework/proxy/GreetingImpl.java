04a3ca0ad4b352b854c993cc6bded9a026cd4983
lemonliu1993 <2305247091@qq.com>
2020-03-21 11:43:35 +0800

    从零开始javaweb——自动代理(扫码切面配置)

--- smart-framework/src/main/java/com/lemon/framework/proxy/GreetingImpl.java
+++ smart-framework/src/main/java/com/lemon/framework/proxy/GreetingImpl.java
@@ -1,13 +1,17 @@
 package com.lemon.framework.proxy;
 
+import com.lemon.framework.annotation.Service;
+
 /**
  * Created by lemoon on 2020/3/19 7:40 PM
  */
-public class GreetingImpl implements Greeting{
+
+@Service
+public class GreetingImpl implements Greeting {
 
     public void sayHello(String name) {
         before();
-        System.out.println("Hello!"+name);
+        System.out.println("Hello!" + name);
         after();
     }
 
@@ -18,5 +22,13 @@ public class GreetingImpl implements Greeting{
 
     private void before() {
         System.out.println("Before");
+    }
+
+    public void goodMorning(String name) {
+        System.out.println("Good Morning! " + name);
+    }
+
+    public void goodNight(String name) {
+        System.out.println("Good Night! " + name);
     }
 }
