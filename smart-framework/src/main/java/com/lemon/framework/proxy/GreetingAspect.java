30a5238f8d4c4b49946284462f5181451a9eb9b0
lemonliu1993 <2305247091@qq.com>
2020-03-23 14:27:15 +0800

    从零开始javaweb——aspectJ注解说明

--- smart-framework/src/main/java/com/lemon/framework/proxy/GreetingAspect.java
+++ smart-framework/src/main/java/com/lemon/framework/proxy/GreetingAspect.java
@@ -7,13 +7,16 @@ import org.springframework.stereotype.Component;
 
 /**
  * Created by lemoon on 2020/3/22 7:53 PM
+ * 类上面标注的Aspect注解表明该类是一个Aspect(其实就是Advisor)。该类无需实现任何的接口，只需定义一个方法(方法名叫什么都无所谓)
+ * 在方法上标注@Around注解，在注解中使用Aspect切点表达式。方法中的参数中包括一个ProceedingJointPoint对象，
+ * 它在AOP中称为JointPoint(连接点)，可以通过该对象获取方法的任何信息,例如，方法名，参数等
  */
 
 @Aspect
 @Component
 public class GreetingAspect {
 
-    @Around("execution(*aop.demo.GreetingImpl.*(,,,))")
+    @Around("execution(* com.lemon.framework.proxy.GreetingImpl.*(..))")
     public Object arount(ProceedingJoinPoint pjp) throws Throwable {
         before();
         Object result = pjp.proceed();
