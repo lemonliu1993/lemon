0854d364ab73894b98f4b7691eb94466ead4ea0d
lemonliu1993 <2305247091@qq.com>
2020-05-07 18:57:44 +0800

    设计模式——组合关系

---
+++ DesignPattern/src/com/lemon/principle/composite/Computer1.java
@@ -0,0 +1,10 @@
+package com.lemon.principle.composite;
+
+/**
+ * Created by lemoon on 2020/5/7 6:54 PM
+ */
+public class Computer1 {
+
+    private Mouse mouse = new Mouse();
+    private Monitor monitor = new Monitor();
+}
