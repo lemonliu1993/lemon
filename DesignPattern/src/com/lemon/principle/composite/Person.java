f6587d1cea213e3f88378d3c8a1c4badc7482c55
lemonliu1993 <2305247091@qq.com>
2020-05-08 22:55:45 +0800

    设计模式——组合关系

---
+++ DesignPattern/src/com/lemon/principle/composite/Person.java
@@ -0,0 +1,18 @@
+package com.lemon.principle.composite;
+
+/**
+ * Created by lemoon on 2020/5/8 10:54 PM
+ */
+public class Person {
+
+    private IDCard card;
+    private Head head = new Head();
+}
+
+class IDCard {
+
+}
+
+class Head {
+
+}
